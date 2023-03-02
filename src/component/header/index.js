861c4b6b9b032b42f3c74044bfc1dd545d4cbd81
hanif ramadhani <mhaniframadhani985@gmail.com>
2023-03-02 10:55:56 +0700

    perbaikan untuk testing

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className=" bg-out-of-blue-900 text-white font-bold uppercase px-8 sm:px-16 md:px-20 lg:px-30 2xl:px-56 py-5 sm:py-6 md:py-7 shadow-md">
-      <h1 className="text-2xl">to do list app</h1>
+      <h1 className="text-2xl" data-cy="header-title">to do list app</h1>
     </header>
   )
 }
